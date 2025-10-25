3beb8fabdd5b6eaccca040decdc31de4eff421b5
ALHAN RENDON RUIZ <alhanrendon1118935@correo.itm.edu.co>
2025-10-25 14:55:00 -0500

    agregar dependencias al pom.xml, crear una clase principal llamada ProyectoLockersApplication.java, modificar LockerController.java

---
+++ src/main/java/com/lockers/Application/ProyectoLockersApplication.java
@@ -0,0 +1,11 @@
+package com.lockers.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLockersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoLockersApplication.class, args);
+    }
+}
